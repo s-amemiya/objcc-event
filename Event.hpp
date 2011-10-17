865c285581a509aa9056f2d1aaff72ee1b5a5281
Shinya Amemiya <shinya@Ged.local>
2011-10-18 05:34:14 +0900

    add Event.hpp

---
+++ Event.hpp
@@ -0,0 +1,73 @@
+
+#ifndef EVENT_HPP
+#define EVENT_HPP
+
+#include <vector>
+#include <algorithm>
+#include <boost/foreach.hpp>
+
+template <typename BlockType>
+class EventBase
+{
+public:
+	void operator+=(BlockType block)
+	{
+		blocks_.push_back(block);
+	}
+	
+	void operator-=(BlockType block)
+	{
+		blocks_.erase(
+					  std::remove(blocks_.begin(), blocks_.end(), block),
+					  blocks_.end());
+	}
+	
+protected:
+	std::vector<BlockType> blocks_;
+};
+
+template <class Arg1Type = void, class Arg2Type = void, class Arg3Type = void>
+struct Event : public EventBase<void(^)(Arg1Type, Arg2Type, Arg3Type)>
+{
+	void operator()(const Arg1Type& arg1, const Arg2Type& arg2, const Arg3Type& arg3) const
+	{
+		BOOST_FOREACH (void(^block)(Arg1Type, Arg2Type, Arg3Type), this->blocks_) {
+			block(arg1, arg2, arg3);
+		}
+	}
+};
+
+template <class Arg1Type, class Arg2Type>
+struct Event<Arg1Type, Arg2Type, void> : public EventBase<void(^)(Arg1Type, Arg2Type)>
+{
+	void operator()(const Arg1Type& arg1, const Arg2Type& arg2) const
+	{
+		BOOST_FOREACH (void(^block)(Arg1Type, Arg2Type), this->blocks_) {
+			block(arg1, arg2);
+		}
+	}
+};
+
+template <class Arg1Type>
+struct Event<Arg1Type, void, void> : public EventBase<void(^)(Arg1Type)>
+{
+	void operator()(const Arg1Type& t) const
+	{
+		BOOST_FOREACH (void(^block)(Arg1Type), this->blocks_) {
+			block(t);
+		}
+	}
+};
+
+template <>
+struct Event<void, void, void> : public EventBase<void(^)(void)>
+{
+	void operator()() const
+	{
+		BOOST_FOREACH (void(^block)(void), this->blocks_) {
+			block();
+		}
+	}
+};
+
+#endif // EVENT_HPP
